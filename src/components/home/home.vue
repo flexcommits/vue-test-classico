c1033c1cd53a9c2bd896ff8f84780d1de6b4c9f9
flexcommits <stevenlee199571@gmail.com>
2024-02-15 20:30:28 -0500

    fixed displaying for img

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -66,7 +66,7 @@
                                         <template v-if="selectedNavItem==game.type" v-for="(item,i) in game.list">
                                             <div class="game-item no-animate" v-if="i < 6" @click="toGame(item.gameid, item.type, item.roomid, item.open, item)">
                                                 <img class="game-img" v-if="game.type == 3" :src="'https://lucas999.prerelease-env.biz/game_pic/square/200/'+item.gameid+'.png'" />
-                                                <img class="game-img" v-else :src="sourceUrl+item.roomid+'_'+GLOBAL.lanCode+'.png'" />
+                                                <img class="game-img" v-else :src="require('../../assets/hot/'+item.roomid+'_'+GLOBAL.lanCode+'.png')" />
                                             </div>
                                         </template>
                                     </template>
@@ -105,7 +105,7 @@
                                     <template v-for="(item,i) in allGame.list">
                                         <div class="game-item no-animate" @click="toGame(item.gameid, item.type, item.roomid, item.open, item)">
                                             <img class="game-img" v-if="allGame.type == 3" :src="'https://lucas999.prerelease-env.biz/game_pic/square/200/'+item.gameid+'.png'" />
-                                            <img class="game-img" v-else :src="sourceUrl+item.roomid+'_'+GLOBAL.lanCode+'.png'" />
+                                            <img class="game-img" v-else :src="require('../../assets/hot/'+item.roomid+'_'+GLOBAL.lanCode+'.png')" />
                                         </div>
                                     </template>
                                 </div>
